2b0cfe5c6489f8c9d8e0e547660586c426e35fc7
Tejas Indulal Dhamecha <tidhamecha@in.ibm.com>
2022-10-23 11:01:49 +0530

    my submission for the quiz 3

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -55,6 +55,43 @@ def test_tune_and_save():
     assert os.path.exists(actual_model_path)
     assert type(load(actual_model_path)) == type(clf)
 
+
+def test_not_biased():    
+    h_param_comb = helper_h_params()
+    x_train, y_train = helper_create_bin_data(n=100, d=7)
+    x_dev, y_dev = x_train, y_train
+    x_test, y_test = x_train, y_train
+
+    clf = svm.SVC()
+    metric = metrics.accuracy_score
+    
+    model_path = "test_run_model_path.joblib"
+    actual_model_path = tune_and_save(clf, x_train, y_train, x_dev, y_dev, metric, h_param_comb, model_path)
+    best_model = load(actual_model_path)
+
+    predicted = best_model.predict(x_test)
+
+    assert len(set(predicted))!=1
+
+
+def test_predicts_all():    
+    h_param_comb = helper_h_params()
+    x_train, y_train = helper_create_bin_data(n=100, d=7)
+    x_dev, y_dev = x_train, y_train
+    x_test, y_test = x_train, y_train
+
+    clf = svm.SVC()
+    metric = metrics.accuracy_score
+    
+    model_path = "test_run_model_path.joblib"
+    actual_model_path = tune_and_save(clf, x_train, y_train, x_dev, y_dev, metric, h_param_comb, model_path)
+    best_model = load(actual_model_path)
+
+    predicted = best_model.predict(x_test)
+
+    assert set(predicted) == set(y_test)
+
+
 # what more test cases should be there
 # irrespective of the changes to the refactored code.
 
